149ba830b0b7ea943530eb5bdcf3228e8f7b3c39
Amadeus Kurisu <Nerupo@chan>
2018-12-20 20:20:53 -0500

    update

--- mobile/src/main/java/com/colorxy/www/colorxy/Library/ColorLibrary.java
+++ mobile/src/main/java/com/colorxy/www/colorxy/Library/ColorLibrary.java
@@ -1,4 +1,18 @@
 package com.colorxy.www.colorxy.Library;
 
-public class ColorLibrary {
+import java.util.ArrayList;
+
+/**
+ * Library that stores color (Singleton)
+ * @author Tobknight
+ */
+public class ColorLibrary implements ColorInterface{
+    /**Instance of Color Library*/
+    private ColorLibrary instance;
+
+    /**Shelf*/
+    private ArrayList<Color> ColorShelf;
+
+
+
 }
--- mobile/src/main/java/com/colorxy/www/colorxy/Library/PlatterManager.java
+++ mobile/src/main/java/com/colorxy/www/colorxy/Library/PlatterManager.java
@@ -1,7 +1,11 @@
 package com.colorxy.www.colorxy.Library;
 
 /**
+ * Platter Manager save platters
  * @author Tobknight
  */
 public class PlatterManager {
+
+    
+
 }
