77f4b7cbb942c61d955401b72b3a0d344c047bfb
Amadeus Kurisu <Nerupo@chan>
2018-12-20 22:30:50 -0500

    update

--- mobile/src/main/java/com/colorxy/www/colorxy/Library/ColorLibrary.java
+++ mobile/src/main/java/com/colorxy/www/colorxy/Library/ColorLibrary.java
@@ -1,18 +1,73 @@
 package com.colorxy.www.colorxy.Library;
 
+import android.graphics.Color;
+
 import java.util.ArrayList;
 
 /**
  * Library that stores color (Singleton)
+ *
  * @author Tobknight
  */
-public class ColorLibrary implements ColorInterface{
-    /**Instance of Color Library*/
+public class ColorLibrary implements ColorInterface {
+    /**
+     * Instance of Color Library
+     */
     private ColorLibrary instance;
 
-    /**Shelf*/
+    /**
+     * Shelf
+     */
     private ArrayList<Color> ColorShelf;
 
 
+    public ColorLibrary getInstance() {
+        if (instance == null) {
+            instance = new ColorLibrary();
+        }
+        return instance;
+    }
 
+    /**
+     * Constructor
+     */
+    private ColorLibrary() {
+        ColorShelf = new ArrayList<Color>();
+    }
+
+    /**
+     * Add color in RGB
+     *
+     * @param R Red
+     * @param G Green
+     * @param B Blue
+     */
+    @Override
+    public void addRGB(float R, float G, float B) {
+
+    }
+
+    /**
+     * Add color in HSL
+     *
+     * @param H HUE
+     * @param S Saturation
+     * @param L Lightness
+     */
+    @Override
+    public void addHSL(float H, float S, float L) {
+
+    }
+
+    /**
+     * Add color in HSV
+     *
+     * @param H HUE
+     * @param S Saturation
+     * @param V Value
+     */
+    @Override
+    public void addHSV(float H, float S, float V) {
+
+    }
 }
